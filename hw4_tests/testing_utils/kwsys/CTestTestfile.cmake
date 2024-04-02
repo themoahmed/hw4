833e748bf98f08e0abfc21ce3203fb383aefcb9e
Mohamed Ahmed <ahmedma@usc.edu>
2024-04-01 23:43:27 -0700

    removed tests

--- hw4_tests/testing_utils/kwsys/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /work/Homework/hw4/hw4_tests/testing_utils/kwsys
-# Build directory: /work/Homework/hw4/hw4_tests/testing_utils/kwsys
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
